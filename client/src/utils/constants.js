b7079324e44bff1d3b2e107820dac3e85984319d
Suhas <suhasmaruthi777@gmail.com>
2026-08-01 12:30:01 +0530

    fix: use dynamic VITE_CONTRACT_ADDRESS and V10 ABI in frontend

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,12 +1,12 @@
-import UploadUpgradeableV9 from "./UploadUpgradeableV9.json";
+import UploadUpgradeableV10 from "./UploadUpgradeableV10.json";
 import DriveDAO from "../abis/DriveDAO.json";
 import DriveToken from "../abis/DriveToken.json";
 import DriveFaucet from "../abis/DriveFaucet.json";
 
 
 
-export const contractAddress = "0x71D9B51bFE5DE572673B241B1f9109e58F0B834F"; // Automatically use new V9 proxy
-export const contractAbi = UploadUpgradeableV9.abi || UploadUpgradeableV9;
+export const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+export const contractAbi = UploadUpgradeableV10.abi || UploadUpgradeableV10;
 export const daoAddress = import.meta.env.VITE_DAO_ADDRESS;
 export const daoAbi = DriveDAO.abi || DriveDAO;
 export const tokenAddress = import.meta.env.VITE_TOKEN_ADDRESS;
